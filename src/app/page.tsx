fdab83b85906e80114cf7bb9637a9f648936b996
anythinks <alauddinazhary05@gmail.com>
2025-11-08 00:47:13 +0700

    add suspense in "/"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 "use client";
 
+import { Suspense } from "react";
 import NoticePage from "./ui/home/home";
+import { HomeSkeleton } from "./ui/home/home-skeleton";
 
 export default function Home() {
-  return <NoticePage noticeType="red" />;
+  return (
+    <Suspense fallback={<HomeSkeleton />}>
+      <NoticePage noticeType="red" />;
+    </Suspense>
+  );
 }
